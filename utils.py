893ffa2827a6d467553c62124bf9eca684397f02
zoharli <zoharg@outlook.com>
2017-03-31 21:53:13 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,8 +9,8 @@ def conv_layer(input,filter,stride,name='conv'):
 def batch_norm(input,name='BN'):
     ch_num=input.shape.as_list()[3]
     return tf.nn.fused_batch_norm(input,
-                                  offset=tf.Variable(tf.zeros(ch_num),name='offset'),
-                                  scale=tf.Variable(tf.ones(ch_num),name='scale'),
+                                  offset=tf.Variable(tf.zeros(ch_num),name=name+'/offset'),
+                                  scale=tf.Variable(tf.ones(ch_num),name=name+'/scale'),
                                   name=name)[0]
 def conv_filter(shape,name='filter'):
     return tf.Variable(tf.truncated_normal(shape),name=name)
